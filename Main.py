ee662e851b0058020725140c8e948325dc2315cb
wanghai <nicewanghai@163.com>
2022-09-09 15:13:05 +0800

    # 第2次提交 没有一点点的头绪....

---
+++ Main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time : 2022/9/9 14:33
+# @Author : Wang Hai
+# @Email : nicewanghai@163.com
+# @Code Specification : PEP8
+# @File : Main.py
+# @Project : HaiHai
+
+
+if __name__ == "__main__":
+    while True:
+        others_words = input()
+        print("知否知否，好一个绿肥红瘦")
+
